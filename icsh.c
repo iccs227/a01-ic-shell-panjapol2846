cb66028a20d9c479cc85966133978dcdd8d7f454
Panjapol <panjapol2846@gmail.com>
2025-05-29 14:21:28 +0000

    Milestone 5 finished

--- icsh.c
+++ icsh.c
@@ -1,6 +1,6 @@
 /* ICCS227: Project 1: icsh
- * Name:Panjapol Chayasana
- * StudentID:6580059
+ * Name: Panjapol Chayasana
+ * StudentID: 6580059
  */
 
 #include <stdio.h>
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
 
 #define MAX_LINE 1024
 
@@ -96,14 +97,55 @@ int main(int argc, char *argv[]) {
                 continue;
             } else if (pid == 0) {
                 setpgid(0, 0);
+
                 char *args[MAX_LINE / 2 + 1];
-                args[0] = cmd;
-                int i = 1;
+                int i = 0;
+                int redirect_in = 0, redirect_out = 0;
+                char *input_file = NULL, *output_file = NULL;
+
+                args[i++] = cmd;
                 char *token;
                 while ((token = strtok(NULL, " ")) != NULL) {
-                    args[i++] = token;
+                    if (strcmp(token, "<") == 0) {
+                        redirect_in = 1;
+                        input_file = strtok(NULL, " ");
+                        if (!input_file) {
+                            fprintf(stderr, "Syntax error: no input file\n");
+                            exit(1);
+                        }
+                    } else if (strcmp(token, ">") == 0) {
+                        redirect_out = 1;
+                        output_file = strtok(NULL, " ");
+                        if (!output_file) {
+                            fprintf(stderr, "Syntax error: no output file\n");
+                            exit(1);
+                        }
+                    } else {
+                        args[i++] = token;
+                    }
                 }
                 args[i] = NULL;
+
+                if (redirect_in) {
+                    int fd = open(input_file, O_RDONLY);
+                    if (fd < 0) {
+                        perror("Input file error");
+                        exit(1);
+                    }
+                    dup2(fd, STDIN_FILENO);
+                    close(fd);
+                }
+
+                if (redirect_out) {
+                    int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                    if (fd < 0) {
+                        perror("Output file error");
+                        exit(1);
+                    }
+                    dup2(fd, STDOUT_FILENO);
+                    close(fd);
+                }
+
                 execvp(cmd, args);
                 perror("exec failed");
                 exit(1);
@@ -131,3 +173,5 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+
